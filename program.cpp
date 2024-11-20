110df0c3ec662b6e75c0b51c53625146695c3908
Nikolas <318158@stud.umk.pl>
2024-11-20 19:10:25 +0100

    Dodanie funkcji silnia

--- program.cpp
+++ program.cpp
@@ -2,13 +2,22 @@
 
 using namespace std;
 
+int silnia(int a){
+	if (a == 0){
+		return 1;
+	}
+	else{
+		return a * silnia(a - 1); 
+	}
+}
+
 int main(){
 	
 	int n = 0;
 
 	cin >> n;
 
-	cout << n;
+	cout << silnia(n);
 
 	return 0;
 }
